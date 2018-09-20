ee1a82dd1117b434d852e8ca40713fd04f0e22c5
Lukas Woodtli <woodtli.lukas@gmail.com>
2018-09-20 13:24:15 +0200

    Initial commit for .gitconfig file

---
+++ setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+set -u
+
+
+readonly THIS_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+readonly GITCONFIG_FILE="$HOME/.gitconfig"
+readonly GITCONFIG_FILE_REPO="$THIS_SCRIPT_DIR/.gitconfig"
+
+
+if [ -f "$GITCONFIG_FILE" ]
+then
+	echo ".gitconfig exists: create backup file .gitconfig.bak"
+	mv "$GITCONFIG_FILE" "$GITCONFIG_FILE.bak"
+fi
+
+pushd $HOME &> /dev/null
+ln -s $GITCONFIG_FILE_REPO $GITCONFIG_FILE
+
